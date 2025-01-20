fcca06dead8dfabcdc0589456dfea3610d5385db
Xdsaad5 <xdsaad5@gmail.com>
2025-01-20 15:35:06 +0500

    Fixed issue routerlink not working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { NavComponent } from "./nav/nav.component";
 import { CommonModule } from '@angular/common';
-import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavComponent,RouterOutlet,CommonModule],
+  imports: [HeaderComponent, NavComponent,RouterOutlet,CommonModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
